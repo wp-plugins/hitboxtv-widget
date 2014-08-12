08cf48940bc45226e15b4163cdbb4df42b8b1c6f
MadMakz <MadMakz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-12 07:44:52 +0000

    Single widget can now show multiple channels for better styleing. You can enter a comma (,) seperated list of channels No more mixed code style Replaced category image with stream preview image CSS changes Added screenshot and readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/hitboxtv-widget/tags/1.1@964398 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hitboxtv-widget.php
@@ -0,0 +1,98 @@
+<?php
+/*
+Plugin Name: Hitbox.TV Widget
+Plugin URI: http://wordpress.org/plugins/hitboxtv-widget/
+Description: Hitbox.TV status widget.
+Version: 1.1
+Author: SpiffyTek
+Author URI: http://spiffytek.com/
+License: Copyright (C) 2014 SpiffyTek
+
+http://spiffytek.com/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+class st_hitbox_widget extends WP_Widget {
+	function st_hitbox_widget() {
+        parent::WP_Widget(false, $name = __('Hitbox.TV Widget', 'wp_widget_plugin') );
+    }
+	
+	function form($instance) {
+		if( $instance) {
+			$title = esc_attr($instance['title']);
+			$text = esc_attr($instance['text']);
+		} else {
+			$title = '';
+			$text = '';
+		}
+?>	
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Widget Title', 'wp_widget_plugin'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('text'); ?>"><?php _e('Channel or multiple channels seperated by comma:', 'wp_widget_plugin'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>" type="text" value="<?php echo $text; ?>" />
+		</p>
+<?php
+	}
+	
+	function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['text'] = strip_tags($new_instance['text']);
+		return $instance;
+	}
+	
+	function widget($args, $instance) {
+		extract( $args );
+		$title = apply_filters('widget_title', $instance['title']);
+		$text = $instance['text'];
+		echo $before_widget;
+
+		if ( $title ) {
+			echo $before_title . $title . $after_title;
+		}
+
+		if( $text ) {
+			require_once(plugin_dir_path(__FILE__) . 'includes/functions.php');
+			define('HITBOX_TV_WIDGET_PATH', plugin_dir_path( __FILE__ ));
+			define('HITBOX_TV_WIDGET_URI', plugins_url('', __FILE__));
+			
+			$text = explode( ',', trim($text) );
+			
+			echo '<div class=\"st-hitbox-widget-holder\">
+					<ul>';
+					
+			for( $i = 0; $i <= count($text) - 1; $i++ ){
+				echo _hitbox_status($text[$i]);
+			}
+			
+			echo '</ul>
+				</div>';
+		} else {
+			echo '<p class="wp_widget_plugin_text">No channel set!</p>';
+		}
+		
+		echo $after_widget;
+	}
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("st_hitbox_widget");'));
+add_action('wp_enqueue_scripts', '_sthw_add_stylesheet');
+
+function _sthw_add_stylesheet() {
+	wp_enqueue_style('st-hitbox-widget', plugins_url('style.css', __FILE__));
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,56 @@
+=== Hitbox.TV Widget ===
+Contributors:
+Donate link: http://spiffytek.com/spenden/
+Tags: hitbox, status, live, widget
+Requires at least: 3.9.0
+Tested up to: 3.9.2
+Stable tag: 1.1
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Wordpress widget to show the status of one or multiple Hitbox.tv streams.
+
+== Description ==
+
+Wordpress widget to show the status of one or multiple Hitbox.tv streams.
+
+Features:
+*   Can display multiple channels in one widget
+*   Preview image on online channels
+*   Channel name and preview image linked to hitbox.tv
+*   Displays viewer count
+*   Displays title message
+*   Displays wich game is played
+
+== Installation ==
+
+1. Upload everything into `hitbox-tv-widget` directory to the `/wp-content/plugins/` directory. Keep the structure intact.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Activate/configure the widget in the 'Widget' menu in WordPress.
+
+To show multiple channels in one widget simply add a comma seperated list of channels into the widgets 'Channel' field.
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+
+screenshot.jpg
+
+== Changelog ==
+
+= 1.1 =
+* Single widget can now show multiple channels for better styleing. You can enter a comma (,) seperated list of channels
+* No more mixed code style
+* Replaced category image with stream preview image
+* CSS changes
+* Added screenshot and readme.txt
+
+= 1.0 =
+* First working version
+
+== Arbitrary section ==
+IMPORTANT NOTE:
+
+I'm only hosting the stable code at WordPress!
+For actual development versions please visit http://code.spiffytek.com/wp-hitbox-status-widget
