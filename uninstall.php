956ec4222d5560b4ba8f8fae5b9753176cf75e30
MadMakz <MadMakz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-20 11:46:53 +0000

    http://builds.spiffytek.net/changelogs/wp-hitbox-status-widget.txt
    git-svn-id: https://plugins.svn.wordpress.org/hitboxtv-widget/tags/1.5.0@968923 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Copyright (C) 2014 SpiffyTek
+
+http://spiffytek.com/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+function _sthw_uninstall(){
+	delete_option('sthw_cache_enable');
+	delete_option('sthw_cache_lifetime');
+	delete_option('sthw_hide_unknown');
+}
+?>
